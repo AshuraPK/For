bab9240f10833c5983f7af9589d3727d8f01d2ca
AshuraPK <168103263+AshuraPK@users.noreply.github.com>
2024-08-25 23:29:20 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,49 @@
+-----------------------------------For-------------------------------------------------*/
+
+/*1 - Implemente uma contagem regressiva de 10 até 0 utilizando um laço de repetição. 
+
+for (cont = 10; cont >= 0; cont--){
+   console.log(cont)
+}*/ 
+ 
+/* 2 - Escreva um programa que peça ao usuário para digitar um número e exiba sua tabuada de 1 a 10. 
+ num1 = 0
+  num1 = prompt ("escreva numero")
+for (cont = 0; cont <= 10;  cont++ ){
+ 
+  
+  resultado = num1 * cont
+  console.log (resultado)
+}
+*/
+/*3 - Calcule a soma de todos os números de 1 a 100 utilizando um laço de repetição.
+
+resposta = 0 
+ for (cont = 0; cont <= 100; cont++) {
+      resposta = resposta + cont
+    console.log (resposta)
+ }
+*/
+/* 4 - Imprima todos os números pares de 1 a 20 utilizando um laço de repetição
+
+
+for (cont = 0; cont <= 20; cont++){
+  if (cont %2==0){
+     console.log (cont)
+  }
+}
+*/
+
+/* 5 - Escreva um programa que peça ao usuário para digitar um número e exiba a sequência de Fibonacci até esse número.
+
+num1 = 1
+num2 = 0
+num3 = 0
+numUsuario = prompt ('escreva um numero')
+for(cont =0; cont <= numUsuario; cont++ ){
+  console.log(num3)
+  num3 = num1 + num2 
+  num1 = num2
+  num2 = num3
+}
+*/
